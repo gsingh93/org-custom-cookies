5bea27e6794e6be95a410546833cc1984ea2afed
Gulshan Singh <gsingh2011@gmail.com>
2022-01-18 20:07:19 -0800

    WIP: Update readme and tests

--- README.md
+++ README.md
@@ -42,23 +42,45 @@ CLOCK: [2021-11-05 Fri 12:20]--[2021-11-05 Fri 12:45] =>  0:25
 :END:
 ```
 
-Running `M-x org-custom-cookies-update` with `point` anywhere inside the subtree containing the custom cookies will update the headline containing the cookies to this:
+Running `M-x org-custom-cookies-containing-subtree` with `point` anywhere inside the subtree containing the custom cookies will update the headline containing the cookies to this:
 ```org
 * Agenda for today [S: 1:00] [C: 1:20] [E: 1:30]
 ```
 
 If any changes are made to the scheduled times, clocked times, or effort, running this command again will update the cookie values to the new values.
 
+There are five different functions that can be used for updating custom cookies:
+
+- `org-custom-cookies-update-current-heading`: Update any custom cookies defined in the heading of item point is currently at
+- `org-custom-cookies-update-nearest-heading`: Update any custom cookies defined in the heading of the current item if any exist, otherwise search for the closest parent heading with the custom cookie and update that heading. Any further parent headings are not updated.
+- `org-custom-cookies-update-subtree`: Updates any custom cookies found in the current heading and any child heading of the current heading
+- `org-custom-cookies-update-containing-subtree`: First finds the topmost parent heading that contains a custom cookie, and then updates all custom cookies in headings in that subtree
+- `org-custom-cookies-update-all`: Updates all custom cookies in the buffer
+
+TODO: Give examples of each command above?
 TODO: Mention how to add this command to the org mode-map
+TODO: Mention how to add triggers on effort property change, schedule, and clock out
 
 ### Adding Custom Cookies
 
-Note that the cookie regexes can't match each other
+You can add your own custom cookies by customizing the `org-custom-cookies-alist` variable. The default value is:
 
-`org-custom-cookies-alist`
-- add to list
-- remove from list
-- overwrite entire list
-- change regex for one entry
+```elisp
+(("\\[S: ?\\(?:[0-9]*:[0-9]*\\)?\\]" . org-custom-cookies--subtree-scheduled-duration-cookie)
+    ("\\[C: ?\\(?:[0-9]*:[0-9]*\\)?\\]" . org-custom-cookies--subtree-clocksum-cookie)
+    ("\\[E: ?\\(?:[0-9]*:[0-9]*\\)?\\]" . org-custom-cookies--subtree-effort-cookie))
+```
 
-TODO: Make a note about org-column-mode
+The keys for the `alist` are the regexes to match in a heading. When a regex matches, the corresponding function is called at the beginning of the matching header. The custom cookie that was matched is then replaced by the return value of this function, which itself should also match the regex so the function can be run in the future to update the cookie value. Avoid adding any regexes that could match multiple custom cookie formats, as that will result in one custom cookie function overwriting the result of another custom cookie function.
+
+- add to list (setf + alist-get or just add-to-list?))
+- remove from list (alist-get or assq-delete-all or rassq-delete-all)
+- overwrite entire list (setq)
+- change regex for one entry (setf + alist-get?)
+
+## Comparison to `org-columns`
+
+`org-columns` is often used to achieve what this package does, but there are multiple issues with it. The first issue is purely aesthetic, where some people may not like how the columns look and may prefer the simpler appearance of a cookie instead. The second issue is a lack of customizability in `org-columns`. While `org-columns` does allow you to show the total clocked time, it doesn't allow you to show the total scheduled time, the total effort, or anything other than the built in columns.
+
+TODO: Confirm that org-columns can't show scheduled time or effort
+TODO: Link to manual for org-columns
--- test/test.el
+++ test/test.el
@@ -1,3 +1,6 @@
+;; TODO: Rename tests
+;; TODO: Move .org headings to different test files
+
 (defmacro ert-deftest-matches-expected-file (testname testfile &rest body)
   "TODO"
   `(ert-deftest ,testname ()
@@ -20,11 +23,106 @@
   (should (= (org-custom-cookies--org-timestamp-string-to-duration-seconds "<2021-11-04 Thu 14:30-14:30>") 0))
   (should (= (org-custom-cookies--org-timestamp-string-to-duration-seconds "<2021-11-04 Thu>") 0)))
 
+;; -current-heading while on the heading with a scheduled time cookie
 (ert-deftest-matches-expected-file org-custom-cookies-update-on-heading
  "schedule.org"
- (org-custom-cookies-update))
+ (org-custom-cookies-update-current-heading))
 
+;; -current-heading while inside the heading with a scheduled time cookie
+(ert-deftest-matches-expected-file org-custom-cookies-update-on-heading
+ "schedule.org"
+ (org-next-visible-heading 1)
+ (org-custom-cookies-update-current-heading))
+
+;; -current-heading while inside a subtree scheduled time cookie
+(ert-deftest-matches-expected-file org-custom-cookies-update-on-heading
+ "schedule.org"
+ (org-next-visible-heading 5)
+ (org-custom-cookies-update-current-heading))
+
+;; -current-heading while on the heading with an effort cookie
+(ert-deftest-matches-expected-file org-custom-cookies-update-on-heading
+ "schedule.org"
+ (org-forward-heading-same-level)
+ (org-custom-cookies-update-current-heading))
+
+;; -current-heading while on the heading with a clocked time cookie
+(ert-deftest-matches-expected-file org-custom-cookies-update-on-heading
+ "schedule.org"
+ (org-forward-heading-same-level 2)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -subtree while on subtree with different combinations of cookies
+(ert-deftest-matches-expected-file org-custom-cookies-update-on-heading
+ "schedule.org"
+ (org-forward-heading-same-level 3)
+ (org-custom-cookies-update-current-heading))
+
+;; -nearest-heading
+(ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
+ "schedule.org"
+ (org-forward-heading-same-level 4)
+ (org-next-visible-heading)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -nearest-heading
+(ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
+ "schedule.org"
+ (org-forward-heading-same-level 4)
+ (org-next-visible-heading 2)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -nearest-heading
+(ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
+ "schedule.org"
+ (org-forward-heading-same-level 4)
+ (org-next-visible-heading 3)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -nearest-heading
+(ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
+ "schedule.org"
+ (org-forward-heading-same-level 4)
+ (org-next-visible-heading 4)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -containing-subtree
+(ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
+ "schedule.org"
+ (org-forward-heading-same-level 5)
+ (org-next-visible-heading)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -containing-subtree
+(ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
+ "schedule.org"
+ (org-forward-heading-same-level 5)
+ (org-next-visible-heading 2)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -containing-subtree
+(ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
+ "schedule.org"
+ (org-forward-heading-same-level 5)
+ (org-next-visible-heading 3)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -containing-subtree
+(ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
+ "schedule.org"
+ (org-forward-heading-same-level 5)
+ (org-next-visible-heading 4)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -containing-subtree
+(ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
+ "schedule.org"
+ (org-forward-heading-same-level 5)
+ (org-next-visible-heading 5)
+ (org-custom-cookies-update-nearest-heading))
+
+;; -all
 (ert-deftest-matches-expected-file org-custom-cookies-update-inside-heading
  "schedule.org"
  (goto-char 208)
- (org-custom-cookies-update))
+ (org-custom-cookies-update-all))
