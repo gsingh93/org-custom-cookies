a1e84f91378255164edba3c2b003423398d7c380
tusharhero <tusharhero@sdf.org>
2024-03-19 14:46:44 +0530

    formatting: replace tabs for spaces.

--- org-custom-cookies.el
+++ org-custom-cookies.el
@@ -177,9 +177,9 @@ Hook this function to `org-ctrl-c-ctrl-c-hook' for it to work."
 
 Hook this function to `org-font-lock-set-keywords-hook' for it work."
   (cl-loop for (regex . callback) in org-custom-cookies-alist
-	   do (setq org-font-lock-extra-keywords
-		    (append org-font-lock-extra-keywords
-			    `((,regex (0 'org-checkbox-statistics-todo prepend)))))))
+           do (setq org-font-lock-extra-keywords
+                    (append org-font-lock-extra-keywords
+                            `((,regex (0 'org-checkbox-statistics-todo prepend)))))))
 
 ;;;###autoload
 (defun org-custom-cookies-update-nearest-heading (&optional all)
